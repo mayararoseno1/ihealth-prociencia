90fded54a96fc21e26218d9a53c788a1552456dd
Mayara Roseno <mayararoseno14@gmail.com>
2025-06-27 21:25:06 -0300

    feat: implementação e teste do ShuffleNetV2 concluída

--- train.py
+++ train.py
@@ -9,26 +9,23 @@ from utils.data_loader import get_data_loaders
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Usando dispositivo: {device}")
 
-
 train_loader, test_loader = get_data_loaders(
     train_path='data/raw/training_data',
     test_path='data/raw/testing_data',
     batch_size=32
 )
 
-
 num_classes = len(train_loader.dataset.classes)
 print(f"Número de classes: {num_classes}")
 
+model = models.shufflenet_v2_x1_0(weights=models.ShuffleNet_V2_X1_0_Weights.DEFAULT)
 
-model = models.densenet121(weights=models.DenseNet121_Weights.DEFAULT)
-model.classifier = nn.Linear(model.classifier.in_features, num_classes)
+in_features = model.fc.in_features
+model.fc = nn.Linear(in_features, num_classes)
 model = model.to(device)
-
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=0.001)
-
 
 epochs = 5
 for epoch in range(epochs):
@@ -48,7 +45,7 @@ for epoch in range(epochs):
 
         running_loss += loss.item()
 
-        # Acurácia por época
+        # Acurácia
         _, predicted = torch.max(outputs.data, 1)
         total += labels.size(0)
         correct += (predicted == labels).sum().item()
@@ -56,8 +53,7 @@ for epoch in range(epochs):
     accuracy = 100 * correct / total
     print(f"Época {epoch+1}/{epochs} - Loss: {running_loss/len(train_loader):.4f} - Acurácia: {accuracy:.2f}%")
 
-
+# Salvar modelo treinado
 os.makedirs('models', exist_ok=True)
 torch.save(model.state_dict(), 'models/leukemia_model.pth')
 print("✅ Modelo salvo em models/leukemia_model.pth")
-
