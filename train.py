11b4704431796c035242290a707c7efcf45824dc
Mayara Roseno <mayararoseno14@gmail.com>
2025-07-04 21:37:01 -0300

    feat: treinamento com squeezenet1_0

--- train.py
+++ train.py
@@ -5,7 +5,6 @@ import torch.optim as optim
 from torchvision import models
 from utils.data_loader import get_data_loaders
 
-
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Usando dispositivo: {device}")
 
@@ -18,10 +17,9 @@ train_loader, test_loader = get_data_loaders(
 num_classes = len(train_loader.dataset.classes)
 print(f"Número de classes: {num_classes}")
 
-model = models.shufflenet_v2_x1_0(weights=models.ShuffleNet_V2_X1_0_Weights.DEFAULT)
-
-in_features = model.fc.in_features
-model.fc = nn.Linear(in_features, num_classes)
+model = models.squeezenet1_0(weights=models.SqueezeNet1_0_Weights.DEFAULT)
+model.classifier[1] = nn.Conv2d(512, num_classes, kernel_size=(1, 1))
+model.num_classes = num_classes
 model = model.to(device)
 
 criterion = nn.CrossEntropyLoss()
@@ -45,7 +43,6 @@ for epoch in range(epochs):
 
         running_loss += loss.item()
 
-        # Acurácia
         _, predicted = torch.max(outputs.data, 1)
         total += labels.size(0)
         correct += (predicted == labels).sum().item()
@@ -53,7 +50,6 @@ for epoch in range(epochs):
     accuracy = 100 * correct / total
     print(f"Época {epoch+1}/{epochs} - Loss: {running_loss/len(train_loader):.4f} - Acurácia: {accuracy:.2f}%")
 
-# Salvar modelo treinado
 os.makedirs('models', exist_ok=True)
 torch.save(model.state_dict(), 'models/leukemia_model.pth')
 print("✅ Modelo salvo em models/leukemia_model.pth")
